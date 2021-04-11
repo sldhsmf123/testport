d0ac38e3ef79bf04505b2c6ba2ab527b7a831d18
sldhsmf <sldhsmf@naver.com>
2021-04-11 17:20:08 +0900

    나쁜짓

--- portfolio/script/main.js
+++ portfolio/script/main.js
@@ -1,29 +1,5 @@
 (() => {
     
-    
-    let yOffset = 0;//window.pageYOffset 대신 쓸 변수
-    let prevScrollHeight = 0;//현재 스크롤 위치 yOffset    보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
-    let currentScene = 0; //현재 활성화된 (눈앞에 보고있는 ) 씬(scroll-section) 
-    let enterNewScene = false; //새로운 scene이 시작된 순간 true
-
-    const sceneInfo = [
-        {
-            // 0
-            type: 'sticky',
-            heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
-            scrollHeight: 0,
-            objs: {
-                container: document.querySelector('#scroll-section-0'),
-                messageA: document.querySelector('#scroll-section-0 .main-message.a'),
-                messageB: document.querySelector('#scroll-section-0 .main-message.b'),
-                messageC: document.querySelector('#scroll-section-0 .main-message.c'),
-                messageD: document.querySelector('#scroll-section-0 .main-message.d'),
-                canvas: document.querySelector('#video-canvas-0'),
-                context:document.querySelector('#video-canvas-0').getContext('2d'),
-                videoImages: []
-            },
-            values: {
-                videoImageCount:780,
                 imageSequence: [0, 779],
                 canvas_opcacity:[1, 0, { start: 0.9, end:1 }],
                 messageA_opacity_in: [0, 1, { start: 0.16, end: 0.25 }],
